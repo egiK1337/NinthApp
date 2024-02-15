9daca8f1d2fb1e3170c3ccb5c852767fe15e5863
egiK1337 <117719087+egiK1337@users.noreply.github.com>
2024-02-15 13:47:42 +0300

    Initial commit

---
+++ ImageDownloader.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace NinthApp
+{
+    public class ImageDownloader
+    {
+        public delegate void ImageProcess(string message);
+        public event ImageProcess? ImageStarted;
+        public event ImageProcess? ImageCompleted;
+
+        public async Task Download(string remoteUri, string photoName)
+        {
+            var myWebClient = new WebClient();
+            ImageStarted?.Invoke("Скачивание файла началось");
+            await myWebClient.DownloadFileTaskAsync(remoteUri, photoName);
+            ImageCompleted?.Invoke("Скачивание файла закончилось");   
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿
+namespace NinthApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var imageDownloader = new ImageDownloader();
+            var url = "https://photojournal.jpl.nasa.gov/jpeg/PIA26019.jpg";
+
+            void DisplayMessage(string message) => Console.WriteLine(message);
+
+            imageDownloader.ImageStarted += DisplayMessage;
+            imageDownloader.ImageCompleted += DisplayMessage;
+
+            var dictionaryStatus = new Dictionary<int, bool>();
+            var taskList = new List<Task>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                var task = imageDownloader.Download(url, (i.ToString() + ".jpg"));
+                taskList.Add(task);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Нажмите клавишу <A> для выхода или любую другую клавишу для проверки статуса скачивания");
+                var text = Console.ReadKey().Key;
+                Console.WriteLine();
+
+                if (text == ConsoleKey.A)
+                {
+                    break;
+                }
+                if (text != ConsoleKey.A)
+                {
+                    var count = 0;
+                    var status = "не скачен";
+                    foreach (var item in taskList)
+                    {
+                        if(item.IsCompleted == true)
+                        {
+                            status = "скачен";
+                        }
+                        Console.WriteLine($"фото {count}.jpg в статусе {status}");
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
